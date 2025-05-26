eb4b3aad3e6279d3cc675e295c01160073073784
Geoff Sutcliffe <2geoff@pobox.com>
2025-05-26 17:22:12 +0200

    more on ITV

--- PaperFOF.s
+++ PaperFOF.s
@@ -53,7 +53,7 @@ fof(nc3,negated_conjecture,
     ~ ( ~ s(sK1)
       & ~ q(b)
       & p(c) ),
-    inference(skolemize,[status(esq),new_symbols(skolem,[sK1]),skolemized(X)],[nc2]) ).
+    inference(skolemize,[status(esa),new_symbols(skolem,[sK1]),skolemized(X)],[nc2]) ).
 
 cnf(c1,plain,
     ( q(b)
@@ -102,78 +102,90 @@ cnf(t2,plain,
     ( ~ q(b)
     | ~ p(c)
     | ~ r ),
-    inference(extension,[status(thm),path([t1:1])],[c2]) ).
+    inference(extension,[status(thm),path([t1:1,0:0])],[c2]) ).
 
 cnf(t3,plain,
     $false,
-    inference(connection,[status(thm),path([t2:1,t1:1])],[t2:1,t1:1]) ).
+    inference(connection,[status(thm),path([t2:1,t1:1,0:0])],[t2:1,t1:1]) ).
 
 cnf(t4,plain,
     ( p(c)
     | ~ q(c) ),
-    inference(extension,[status(thm),path([t2:2,t1:1])],[c3]) ).
+    inference(extension,[status(thm),path([t2:2,t1:1,0:0])],[c3]) ).
 
 cnf(t5,plain,
     $false,
-    inference(connection,[status(thm),path([t4:1,t2:2,t1:1])],[t4:1,t2:2]) ).
+    inference(connection,[status(thm),path([t4:1,t2:2,t1:1,0:0])],[t4:1,t2:2]) ).
 
 cnf(t6,plain,
     ( q(c)
     | ~ q(b) ),
-    inference(extension,[status(thm),path([t2:2,t1:1])],[c5]) ).
+    inference(extension,[status(thm),path([t4:2,t2:2,t1:1,0:0])],[c5]) ).
 
 cnf(t7,plain,
     $false,
-    inference(connection,[status(thm),path([t6:1,t4:2,t2:2,t1:1])],[t6:1,t4:2]) ).
+    inference(connection,[status(thm),path([t6:1,t4:2,t2:2,t1:1,0:0])],[t6:1,t4:2]) ).
 
 cnf(t8,plain,
     $false,
-    inference(reduction,[status(thm),path([t6:2,t4:2,t2:2,t1:1])],[t6:2,t1:1]) ).
+    inference(reduction,[status(thm),path([t6:2,t4:2,t2:2,t1:1,0:0])],[t6:2,t1:1]) ).
 
 cnf(l1,plain,
     p(c),
-    inference(lemma,[status(thm),path([t2:2,t1:1]),below(t1:1)],[t4:1]) ).
+    inference(lemma,[status(cth),path([t2:2,t1:1,0:0]),below(t1:1)],[t2:2]) ).
 
 cnf(t9,plain,
     ( r
     | ~ p(c) ),
-    inference(extension,[status(thm),path([t2:3,t1:1])],[c6]) ).
+    inference(extension,[status(thm),path([t2:3,t1:1,0:0])],[c6]) ).
 
 cnf(t10,plain,
     $false,
-    inference(connection,[status(thm),path([t9:1,t2:3,t1:1])],[t9:1,t2:3]) ).
+    inference(connection,[status(thm),path([t9:1,t2:3,t1:1,0:0])],[t9:1,t2:3]) ).
 
 cnf(t11,plain,
+    p(c),
+    inference(lemma_extension,[status(thm),path([t9:2,t2:3,t1:1,0:0])],[l1:1]) ).
+
+cnf(t12,plain,
     $false,
-    inference(lemma_extension,[status(thm),path([l1:1,t9:2,t2:3,t1:1])],[l1:1,t9:2]) ).
+    inference(connection,[status(thm),path([t11:1,t9:2,t2:3,t1:1,0:0])],[t9:2,t11:1]) ).
 
 cnf(l2,plain,
     ~ q(b),
-    inference(lemma,[status(thm),path([t1:1]),below(0:0)],[t2:1]) ).
+    inference(lemma,[status(cth),path([t1:1,0:0]),below(0:0)],[t1:1]) ).
 
-cnf(t12,plain,
+cnf(t13,plain,
     ( s(sK1)
     | q(b)
     | ~ p(c) ),
-    inference(extension,[status(thm),path([t1:2])],[c7]) ).
-
-cnf(t13,plain,
-    $false,
-    inference(connection,[status(thm),path([t12:1,t1:2])],[t12:1,t1:2]) ).
+    inference(extension,[status(thm),path([t1:2,0:0])],[c7]) ).
 
 cnf(t14,plain,
     $false,
-    inference(lemma_extension,[status(thm),path([l2:1,t12:2,t1:2])],[l2:1,t12:2]) ).
+    inference(connection,[status(thm),path([t13:1,t1:2,0:0])],[t13:1,t1:2]) ).
 
 cnf(t15,plain,
-    ( p(c)
-    | q(b) ),
-    inference(extension,[status(thm),path([t12:3,t1:2])],[c4]) ).
+    ~ q(b),
+    inference(lemma_extension,[status(thm),path([t13:2,t1:2,0:0])],[l2:1]) ).
 
 cnf(t16,plain,
     $false,
-    inference(connection,[status(thm),path([t15:1,t12:3,t1:2])],[t15:1,t2:3]) ).
+    inference(connection,[status(thm),path([t15:1,t13:2,t1:2,0:0])],[t15:1,t13:2]) ).
 
 cnf(t17,plain,
+    ( p(c)
+    | q(b) ),
+    inference(extension,[status(thm),path([t13:3,t1:2,0:0])],[c4]) ).
+
+cnf(t18,plain,
     $false,
-    inference(lemma_extension,[status(thm),path([l2:1,t15:2,t12:3,t1:2])],[l2:1,t15:2]) ).
+    inference(connection,[status(thm),path([t17:1,t13:3,t1:2,0:0])],[t17:1,t13:3]) ).
+
+cnf(t19,plain,
+    ~ q(b),
+    inference(lemma_extension,[status(thm),path([t17:2,t13:3,t1:2,0:0])],[l2:1]) ).
+
+cnf(t20,plain,
+    $false,
+    inference(lemma_extension,[status(thm),path([t19:1,t17:2,t13:3,t1:2,0:0])],[l2:1]) ).
